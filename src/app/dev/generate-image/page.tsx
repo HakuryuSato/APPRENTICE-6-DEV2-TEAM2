2e993fa5a21340a0aca79ce357ce1d872d532d40
HakuryuSato <r700540@gmail.com>
2025-01-07 16:59:17 +0900

    未使用のインポートを削除

--- src/app/dev/generate-image/page.tsx
+++ src/app/dev/generate-image/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import React, { useState } from 'react'
-import Image from 'next/image'
 import { fetchGenerateImage } from '@/utils/client/apiClient'
 
 export default function GenerateImagePage() {
