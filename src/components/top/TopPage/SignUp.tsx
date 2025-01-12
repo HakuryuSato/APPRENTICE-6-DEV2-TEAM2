e636347b347c3e42d3b62e766255c1d0e827171c
HakuryuSato <r700540@gmail.com>
2025-01-12 19:41:29 +0900

    SignUpコンポーネントの追加

---
+++ src/components/top/TopPage/SignUp.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSignUp } from '@/hooks/top/TopPage/useSignUp';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+export const SignUp: React.FC = () => {
+  const { inputText, setInputText, handleClickSubmit } = useSignUp();
+  
+  return (
+    <div>
+      <h1>ニックネームの入力</h1>
+      <Input
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder="ニックネーム"
+      />
+      <Button onClick={handleClickSubmit}>はじめる</Button>
+    </div>
+  );
+};
+
+export default SignUp;
