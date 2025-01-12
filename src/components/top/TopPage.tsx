330080ea9389caca503f5aa5af63035ec54aa402
HakuryuSato <r700540@gmail.com>
2025-01-12 19:55:53 +0900

    TopPage配下の各コンポーネントを上下左右中央に配置するようstyleを追加

--- src/components/top/TopPage.tsx
+++ src/components/top/TopPage.tsx
@@ -6,7 +6,7 @@ const temporaryTopPageLayoutMode = 'sign-up';
 
 export const TopPage: React.FC = () => {
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       {/* SignUpと同じように他のコンポーネントもここで条件付きレンダリングします！ */}
       {temporaryTopPageLayoutMode === 'sign-up' && <SignUp />}
     </div>
