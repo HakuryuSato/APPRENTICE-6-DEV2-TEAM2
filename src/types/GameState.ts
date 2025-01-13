3d34b44b269f85a18ef52608332499c27bfe6a5e
HakuryuSato <r700540@gmail.com>
2025-01-13 17:53:47 +0900

    game-state APIで使用するリクエスト/レスポンス用の型を定義

---
+++ src/types/GameState.ts
@@ -0,0 +1,17 @@
+import type { UserStatus } from "./UserStatus";
+
+export type GamePhase = 'prepare' | 'generate' | 'vote' | 'result';
+export type GameAction = 'join' | 'ready';
+
+export interface GameStateResponse {
+    gameId: string;
+    gamePhase: GamePhase;
+    round: number;
+    users: UserStatus[];
+}
+
+export interface GameStateRequest {
+    gameId: string;
+    action: GameAction;
+    users: UserStatus[];
+}
