8e7c2ecffd37b9cd8790ec51dcfe919eed37ad45
HakuryuSato <r700540@gmail.com>
2025-01-13 19:07:08 +0900

    vercelへGameStateを書き込む関数名の一部をchangeからputへ変更

--- src/app/api/game-state/route.ts
+++ src/app/api/game-state/route.ts
@@ -1,13 +1,13 @@
 import { NextRequest } from 'next/server';
 import { handleExternalApiRequest } from '@/utils/server/handleExternalApiRequest';
-import { handleGetGameState, handleChangeGameState } from '@/utils/server/api/gameState';
+import { handleGetGameState, handlePutGameState } from '@/utils/server/api/gameState';
 
 export async function GET(req: NextRequest) {
   return handleExternalApiRequest(() => handleGetGameState(req));
 }
 
 export async function POST(req: NextRequest) {
-  return handleExternalApiRequest(() => handleChangeGameState(req));
+  return handleExternalApiRequest(() => handlePutGameState(req));
 }
 
 
