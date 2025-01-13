cca65e14e90bcc721c69706c745d6a9f29b04632
HakuryuSato <r700540@gmail.com>
2025-01-13 19:41:22 +0900

    GameStateRequest型のプロパティを修正

--- src/types/GameState.ts
+++ src/types/GameState.ts
@@ -1,17 +1,17 @@
-import type { UserStatus } from "./UserStatus";
+import type { UserStatus } from './UserStatus'
 
-export type GamePhase = 'prepare' | 'generate' | 'vote' | 'result';
-export type GameStateRequestType = 'join' | 'ready';
+export type GamePhase = 'prepare' | 'generate' | 'vote' | 'result'
+export type GameStateRequestType = 'join' | 'ready'
 
 export interface GameState {
-    gameId: string;
-    gamePhase: GamePhase;
-    round: number;
-    users: UserStatus[];
+  gameId: string
+  gamePhase: GamePhase
+  round: number
+  users: UserStatus[]
 }
 
 export interface GameStateRequest {
-    gameId: string;
-    gameStateRequestType: GameStateRequestType;
-    users: UserStatus[];
+  gameId: string
+  gameStateRequestType: GameStateRequestType
+  userId: string
 }
