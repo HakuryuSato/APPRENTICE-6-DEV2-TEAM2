0cdd00200d0ef88466b8f7ec85dbecc0e9a3b319
Makoto_Agawa <51506765+mako-agawa@users.noreply.github.com>
2025-01-13 10:27:19 +0900

    add-jotai

--- src/components/top/TopPage/SignUp.tsx
+++ src/components/top/TopPage/SignUp.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { useSignUp } from '@/hooks/top/TopPage/useSignUp';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
+import { useAtom } from 'jotai';
+import { userNameAtom } from '@/atoms/state';
 
 export const SignUp: React.FC = () => {
-  const { inputText, setInputText, handleClickSubmit } = useSignUp();
+  const [userName, setUserName] = useAtom(userNameAtom);
+  console.log(userName); 
+  const { inputText, setInputText, handleClickSubmit } = useSignUp(setUserName);
   
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '16px' }}>
@@ -17,6 +21,7 @@ export const SignUp: React.FC = () => {
         style={{ width: '300px' }}
       />
       <Button onClick={handleClickSubmit}>はじめる</Button>
+    
     </div>
   );
 };
--- src/hooks/top/TopPage/useSignUp.ts
+++ src/hooks/top/TopPage/useSignUp.ts
@@ -1,11 +1,22 @@
+import { topPageModeAtom } from '@/atoms/state';
+import { useAtom } from 'jotai';
 import { useState } from 'react';
 
-export const useSignUp = () => {
+
+export const useSignUp = (setUserName: (name: string) => void) => {
   const [inputText, setInputText] = useState('');
+  const [temporaryTopPageLayoutMode, setTemporaryTopPageLayoutMode] = useAtom(topPageModeAtom); // グローバルステートから取得]
 
   const handleClickSubmit = () => {
-    // ここでinputTextをGlobalStateのuserNameに格納するよう変更
-    console.log(inputText);
+    if (inputText.trim() !== '') {
+      // GlobalStateのuserNameを更新
+      setUserName(inputText);
+      setTemporaryTopPageLayoutMode({ mode: 'select' });
+      console.log('userName updated:', inputText);
+      console.log('temporaryTopPageLayoutMode:', temporaryTopPageLayoutMode);
+    } else {
+      alert('ニックネームを入力してください。');
+    }
   };
 
   return {
