6a0722335bbf314655437530436da1c64781722a
HakuryuSato <r700540@gmail.com>
2025-01-13 20:11:42 +0900

    userIdなどをUserStatusでまとめる

--- src/types/GameState.ts
+++ src/types/GameState.ts
@@ -1,7 +1,7 @@
 import type { UserStatus } from './UserStatus'
 
 export type GamePhase = 'prepare' | 'generate' | 'vote' | 'result'
-export type GameStateRequestType = 'join' | 'ready'
+export type GameStateRequestType = 'create' | 'enter' | 'ready'
 
 export interface GameState {
   gameId: string
@@ -13,5 +13,5 @@ export interface GameState {
 export interface GameStateRequest {
   gameId: string
   gameStateRequestType: GameStateRequestType
-  userId: string
+  userStatus: UserStatus
 }
