e4043419d15b8823fc8dbf5e09a94d1db5f464c0
traning8988 <142848531+traning8988@users.noreply.github.com>
2025-01-15 00:42:38 +0900

    create完成

--- src/components/top/TopPage/Create.tsx
+++ src/components/top/TopPage/Create.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { useAtom } from 'jotai';
-import { gameIdAtom, topPageModeAtom, userNameAtom } from '@/atoms/state';
+import {
+  gameIdAtom,
+  topPageModeAtom,
+  userIdAtom,
+  userNameAtom,
+} from '@/atoms/state';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-// import { fetchGameState, updateGameState } from '@/utils/client/apiClient';
-// import { UserStatus } from '@/types/UserStatus';
+import { fetchGameState, updateGameState } from '@/utils/client/apiClient';
+import { UserStatus } from '@/types/UserStatus';
 
 export const Create: React.FC = () => {
   const router = useRouter();
   const [userName] = useAtom(userNameAtom);
-  // const [userId, setUserId] =useAtom(userIdAtom)
-  const [, setGameId] = useAtom(gameIdAtom);
-  const [temporaryTopPageLayoutMode, setTemporaryTopPageLayoutMode] =
-    useAtom(topPageModeAtom); // グローバルステートから取得
+  const [userId] = useAtom(userIdAtom);
+  const [gameId, setGameId] = useAtom(gameIdAtom);
+  const [, setTemporaryTopPageLayoutMode] = useAtom(topPageModeAtom); // グローバルステートから取得
 
   const [inputText, setInputText] = useState('');
   const [errorText, setErrorText] = useState('');
@@ -28,54 +32,37 @@ export const Create: React.FC = () => {
       setErrorText('任意のゲームIDを入力してください。');
       return;
     }
+    const gameState = await fetchGameState(inputText.trim());
 
-    // const gameState = await fetchGameState(inputText.trim());
-    // console.log("fetchGameState のレスポンス:", gameState)
-    // if (gameState !== null && !(Array.isArray(gameState) && gameState.length === 0) && Object.keys(gameState).length !== 0){
-    //   setErrorText(`${inputText.trim()}は使用中です。別の合言葉を作成してください。`);
-    // } else {
-    //   try {
-    //     const userStatus: UserStatus = {
-    //       userId: userId,
-    //       userName: userName,
-    //       isReady: false,
-    //     };
+    if (gameState !== null) {
+      setErrorText(
+        `${inputText.trim()}は使用中です。別の合言葉を作成してください。`
+      );
+      return;
+    }
 
-    //     console.log("updateGameState に渡すデータ:", {
-    //       gameId: inputText.trim(),
-    //       gameStateRequestType: "create",
-    //       userStatus,
-    //     });
+    const userStatus: UserStatus = {
+      userId: userId,
+      userName: userName,
+      isReady: false,
+    };
 
-    //     const response = await updateGameState({
-    //       gameId: inputText.trim(),
-    //       gameStateRequestType: "create",
-    //       userStatus,
+    const response = await updateGameState({
+      gameId: inputText.trim(),
+      gameStateRequestType: 'create',
+      userStatus,
+    });
 
-    //     });
-
-    //     if (response && response.success) {
-    //       console.log("ゲーム状態の保存に成功:", response);
-    //       // 成功時の処理
-    //       setGameId(inputText.trim());
-    //       router.push(`/game/${inputText.trim()}`);
-    //     } else {
-    //       console.log("ゲーム状態の保存に失敗:", response);
-    //       setErrorText("ゲームの作成に失敗しました。もう一度お試しください。");
-    //     }
-    //   } catch (error) {
-    //     console.error("エラーが発生しました:", error);
-    //     setErrorText("エラーが発生しました。もう一度お試しください。");
-    //   }
-
-    // }
-    setGameId(inputText.trim());
-    router.push(`/game/${inputText.trim()}`);
+    if (response) {
+      setGameId(inputText.trim());
+      router.push(`/game/${gameId}`);
+    } else {
+      setErrorText('ゲームの作成に失敗しました。もう一度お試しください。');
+    }
   };
 
   return (
     <div className="flex flex-col items-center gap-4">
-      <h1>現在は {temporaryTopPageLayoutMode.mode}モードです。</h1>
       <p>ニックネーム: {userName}</p>
 
       <h1>任意のゲームIDを入力してください</h1>
