a4083c045fdce7e80619370ba79e18a4bf2d49b8
HakuryuSato <r700540@gmail.com>
2025-01-14 14:25:18 +0900

    libのvercelKV削除、不要となったため

--- src/lib/vercelKV.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@vercel/kv';
-
-// Vercel KV用クライアント
-export const vercelKVClient = createClient({
-  url: process.env.KV_REST_API_URL, // KV REST APIのURL
-  token: process.env.KV_REST_API_TOKEN, // KV REST APIのトークン
-});
-
