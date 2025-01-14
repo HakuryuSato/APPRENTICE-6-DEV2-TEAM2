dc6e7f335637efef7b10fb1e65be8ac95f652e9e
haruki1227041209 <haruki122704@gmail.com>
2025-01-14 14:15:37 +0900

    コメント追加

--- src/components/game/GeneratedImage.tsx
+++ src/components/game/GeneratedImage.tsx
@@ -6,7 +6,9 @@ import { GeneratedImageProps } from "@/types/GenerateImage";
 export const GeneratedImage: React.FC<GeneratedImageProps> = ({ imageUrl }) => {
   return (
     <div className="w-full">
-      {/* ratioで画像の比率を変えられます */}
+      {/* ratioで画像の比率を変えられます
+          使用する際に、next.config.tsに、許可する画像のホスト名を追加する必要があります
+      */}
       <AspectRatio ratio={4 / 3}>
         <Image
           src={imageUrl}
