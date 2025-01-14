3d7fa3d1bbc61d732edaf00f494448db95f54bee
HakuryuSato <r700540@gmail.com>
2025-01-14 17:15:45 +0900

    route.tsの修正

--- src/app/api/game-state/route.ts
+++ src/app/api/game-state/route.ts
@@ -1,18 +1,19 @@
-import { NextRequest } from 'next/server';
-import { handleExternalApiRequest } from '@/utils/server/handleExternalApiRequest';
-import { handleGetGameState, handlePOSTGameState,handleDeleteGameState } from '@/utils/server/api/gameState';
+import { NextRequest } from 'next/server'
+import { handleExternalApiRequest } from '@/utils/server/handleExternalApiRequest'
+import {
+  handleGetGameState,
+  handlePOSTGameState,
+  handleDeleteGameState
+} from '@/utils/server/api/gameState'
 
-export async function GET(req: NextRequest) {
-  return handleExternalApiRequest(() => handleGetGameState(req));
+export async function GET (req: NextRequest) {
+  return handleExternalApiRequest(() => handleGetGameState(req))
 }
 
-export async function POST(req: NextRequest) {
-  return handleExternalApiRequest(() => handlePOSTGameState(req));
+export async function POST (req: NextRequest) {
+  return handleExternalApiRequest(() => handlePOSTGameState(req))
 }
 
-export async function DELETE(req: NextRequest) {
-  return handleExternalApiRequest(() => handleDeleteGameState(req));
+export async function DELETE (req: NextRequest) {
+  return handleExternalApiRequest(() => handleDeleteGameState(req))
 }
-
-
-
