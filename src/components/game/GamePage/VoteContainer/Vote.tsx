9a1a57077f150ea779d06e8c1887296fc85ef4d4
haruki1227041209 <haruki122704@gmail.com>
2025-01-14 23:58:49 +0900

    voteコンポーネントのUI途中まで

--- src/components/game/GamePage/VoteContainer/Vote.tsx
+++ src/components/game/GamePage/VoteContainer/Vote.tsx
@@ -1,35 +1,38 @@
-import React, { useState } from "react";
-import { GeneratedImage } from "../../GeneratedImage";
+import React, { useState } from 'react';
+import { GeneratedImage } from '../../GeneratedImage';
 
 interface VoteProps {
   imageUrl: string;
   handleVoted?: () => void;
+  isSelected: boolean;
+  onTap: () => void;
 }
 
-export const Vote: React.FC<VoteProps> = ({ handleVoted, imageUrl }) => {
-  const [isNotTapped, setIsNotTapped] = useState(true);
-
+export const Vote: React.FC<VoteProps> = ({
+  handleVoted,
+  imageUrl,
+  isSelected,
+  onTap,
+}) => {
   const handleTap = () => {
-    if (!isNotTapped) {
-      // 2回目のタップで handleVoted を発火
-      if (handleVoted) {
-        handleVoted();
-      }
+    if (!isSelected) {
+      onTap(); // 現在の選択状態を親に通知
+    } else if (handleVoted) {
+      handleVoted(); // 選択状態で2回目のタップ時に発火
     }
-    setIsNotTapped(!isNotTapped);
   };
 
   return (
     <div
       className={`flex flex-col items-center space-y-4 ${
-        handleVoted ? "cursor-pointer" : "opacity-50"
+        handleVoted ? 'cursor-pointer' : 'opacity-50'
       }`}
       role="button"
       onClick={handleTap}
     >
       <div className="relative w-full h-full rounded-md overflow-hidden">
-        <GeneratedImage imageUrl={imageUrl} />
-        {handleVoted && !isNotTapped && (
+        <GeneratedImage url={imageUrl} />
+        {handleVoted && isSelected && (
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <span className="text-white text-xs font-bold">投票する</span>
           </div>
