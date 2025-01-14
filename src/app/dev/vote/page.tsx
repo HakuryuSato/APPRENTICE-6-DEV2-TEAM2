55674f840220d66f055acb0336ce9dd1e86a805e
haruki1227041209 <haruki122704@gmail.com>
2025-01-14 17:09:17 +0900

    Voteコンポーネント作成

---
+++ src/app/dev/vote/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Vote } from "@/components/game/GamePage/VoteContainer/Vote";
+
+export default function VotePage() {
+  const handleVoted = () => {
+    console.log("投票されたよ");
+  };
+  return (
+    <div className="grid grid-cols-2 gap-4 m-4">
+      <Vote
+        handleVoted={handleVoted}
+        imageUrl="https://images.unsplash.com/photo-1736010755388-68a7d4cc0d62?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Vote
+        handleVoted={handleVoted}
+        imageUrl="https://images.unsplash.com/photo-1736010755388-68a7d4cc0d62?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Vote
+        handleVoted={handleVoted}
+        imageUrl="https://images.unsplash.com/photo-1736010755388-68a7d4cc0d62?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+      <Vote imageUrl="https://images.unsplash.com/photo-1736010755388-68a7d4cc0d62?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+    </div>
+  );
+}
