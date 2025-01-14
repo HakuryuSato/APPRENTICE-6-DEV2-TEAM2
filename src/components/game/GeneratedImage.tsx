c150828c80e40867b050d0a2e91929b1b331508f
haruki1227041209 <haruki122704@gmail.com>
2025-01-14 14:09:30 +0900

    generatedImageのUIコンポーネント作成

--- src/components/game/GeneratedImage.tsx
+++ src/components/game/GeneratedImage.tsx
@@ -1,7 +1,20 @@
+import React from "react";
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { GeneratedImageProps } from "@/types/GenerateImage";
 
-export const GeneratedImage: React.FC = () => {
+export const GeneratedImage: React.FC<GeneratedImageProps> = ({ imageUrl }) => {
   return (
-    <>
-    </>
+    <div className="w-full">
+      {/* ratioで画像の比率を変えられます */}
+      <AspectRatio ratio={4 / 3}>
+        <Image
+          src={imageUrl}
+          alt="Generated Image"
+          className="rounded-md object-cover"
+          fill
+        />
+      </AspectRatio>
+    </div>
   );
 };
