ca7224fbb9ef4f1154c53e3761fdbc5609829446
HakuryuSato <r700540@gmail.com>
2025-01-14 14:25:38 +0900

    vercelKVHandlerに読み取り、書き込み、削除用関数を作成

--- src/utils/server/vercelKVHandler.ts
+++ src/utils/server/vercelKVHandler.ts
@@ -1,47 +1,54 @@
-// VercelKV用の関数群
-import { Redis } from '@upstash/redis' 
-import type { GameState } from '@/types/GameState'
+import { kv } from '@vercel/kv'
 
-// VercelKVはVercel上でRedisを使用します
-const redis = Redis.fromEnv();
-
-/**
- * VercelKVから値を取得する関数
- * @param {string} key - 引数:key
- * @returns {Promise<object | null>} 値が存在する場合はデータを、存在しない場合はnullを返す
- * @throws {Error} エラーの場合はエラーを返す
- */
-async function readVercelKV(key: string): Promise<object | null> {
+// VercelKV関連の関数群  -------------------------------------------------
+// 共通関数
+async function handleKvOperation (
+  operation: 'get' | 'set' | 'del',
+  key: string,
+  obj?: object
+): Promise<object | void | null> {
   try {
-    const value = await redis.get(key);
-    return value ?? null;
+    switch (operation) {
+      case 'get':
+        return (await kv.get(key)) ?? null
+      case 'set':
+        // 今回はオブジェクト以外格納予定がないためvalueは''
+        if (obj) {
+          await kv.set(key, '', obj)
+        } else {
+          throw new Error('Missing object for "set" operation')
+        }
+        break
+      case 'del':
+        await kv.del(key)
+        break
+      default:
+        throw new Error(`Unsupported operation: ${operation}`)
+    }
   } catch (error) {
-    console.error('Error reading from VercelKV:', error);
-    throw error;
+    console.error(`Error during VercelKV operation (${operation}):`, error)
+    throw error
   }
 }
 
-// VercelKVに値を書き込む関数
-async function writeVercelKV(key: string, value: object): Promise<void> {
-  try {
-    await redis.set(key, value);
-  } catch (error) {
-    console.error('Error writing to VercelKV:', error);
-    throw error;
-  }
+// 読み取り
+async function kvRead (key: string): Promise<object | null> {
+  return (await handleKvOperation('get', key)) as object | null
+}
+
+// 書き込み
+async function kvWrite (key: string, obj: object): Promise<void> {
+  await handleKvOperation('set', key, obj)
+}
+
+// 削除
+async function kvDelete (key: string): Promise<void> {
+  await handleKvOperation('del', key)
 }
 
 
-// GameStateを取得する関数
-// gameIdを引数としGameStateまたはnullを返す
-async function getGameState(gameId: string): Promise<GameState | null> {
-  return await readVercelKV(gameId) as GameState | null;
+export {
+  kvWrite,
+  kvRead,
+  kvDelete
 }
-
-// GameStateを作成または更新する関数
-// 渡されたGameStateからgameIdを取得してキーとし、GameStateをputする
-async function putGameState(gameState: GameState): Promise<void> {
-  await writeVercelKV(gameState.gameId, gameState);
-}
-
-export { writeVercelKV, readVercelKV, getGameState, putGameState };
