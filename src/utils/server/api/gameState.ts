1bafc55d9e4c7986067c0a40bcc58f5deb810ae5
HakuryuSato <r700540@gmail.com>
2025-01-14 19:15:14 +0900

    gameState.tsをリファクタリング

--- src/utils/server/api/gameState.ts
+++ src/utils/server/api/gameState.ts
@@ -2,13 +2,11 @@ import { NextRequest, NextResponse } from 'next/server'
 import type { GameState, GameStateRequest } from '@/types/GameState'
 import type { UserStatus } from '@/types/UserStatus'
 import { kvGet, kvSet, kvDel } from '@/utils/server/vercelKVHandler'
-import { handleExternalApiRequest } from '@/utils/server/handleExternalApiRequest'
 
 // 汎用関数  ---------------------------------------------------------------------------------------------------
 // GameState更新用共通関数
 async function handleSetGameState (gameState: GameState) {
   await kvSet(gameState.gameId, gameState)
-  return NextResponse.json({}, { status: 200 })
 }
 
 // エラー処理用共通関数
@@ -33,95 +31,95 @@ export async function handleGetGameState (req: NextRequest) {
     return respondWithError('Missing gameId', 400)
   }
 
-  let gameState = await kvGet(gameId)
+  const gameState = await kvGet(gameId)
 
   // gameStateが存在すればgameStateを、存在しなければnullを返す
-  if (!gameState) {
-    gameState = null
-  }
-
-  return gameState
+  return gameState ?? null
 }
 
 // POST  -------------------------------------------------
 export async function handlePOSTGameState (req: NextRequest) {
-  return handleExternalApiRequest(async () => {
-    // 展開
-    const { gameId, gameStateRequestType, userStatus } =
-      (await req.json()) as GameStateRequest
+  // 展開
+  const { gameId, gameStateRequestType, userStatus } =
+    (await req.json()) as GameStateRequest
 
-    // gameIdまたはgameStateRequestTypeがなければエラー
-    if (!gameId || !gameStateRequestType) {
-      return respondWithError('Missing gameId', 400)
-    }
+  // gameIdまたはgameStateRequestTypeがなければエラー
+  if (!gameId || !gameStateRequestType) {
+    return respondWithError('Missing gameId', 400)
+  }
 
-    // gameStateの取得
-    let gameState = (await kvGet(gameId)) as GameState
+  // gameStateの取得
+  let gameState = (await kvGet(gameId)) as GameState
 
-    // RequestTypeに応じて処理
-    switch (gameStateRequestType) {
-      // 作成
-      case 'create': {
-        // 既にgameStateが存在するならばエラー
-        if (gameState) return respondWithError('Already Exists', 409)
+  // create以外でGameStateが存在しないならエラー
+  if (!gameState && gameStateRequestType !== 'create') {
+    return respondWithError('Missing GameState', 400)
+  }
 
-        gameState = {
-          gameId: gameId,
-          gamePhase: 'prepare',
-          round: 0,
-          users: [
-            // 部屋作成者を追加
-            {
-              userId: userStatus.userId,
-              userName: userStatus.userName,
-              isReady: true
-            }
-          ],
-          isAllUsersReady: false
-        } as GameState
+  // RequestTypeに応じて処理
+  switch (gameStateRequestType) {
+    // 作成
+    case 'create': {
+      // 既にgameStateが存在するならば終了
+      if (gameState) break
 
-        await kvSet(gameId, gameState)
-        break
-      }
-
-      // ユーザーの追加
-      case 'enter': {
-        // userIdまたはuserNameがなければエラー
-        if (!userStatus.userId || !userStatus.userName) {
-          return respondWithError('playerId not found', 404)
-        }
-
-        gameState.users.push({
-          userId: userStatus.userId,
-          userName: userStatus.userName,
-          isReady: true
-        })
-
-        await handleSetGameState(gameState)
-        break
-      }
-
-      // ユーザーの準備完了
-      case 'ready': {
-        if (userStatus) {
-          const user = gameState.users.find(
-            user => user.userId === userStatus.userId
-          )
-          if (user) {
-            // 参照渡し
-            user.isReady = true
-            gameState.isAllUsersReady = getAllReady(gameState.users)
-            await handleSetGameState(gameState)
+      gameState = {
+        gameId: gameId,
+        gamePhase: 'prepare',
+        round: 0,
+        users: [
+          // 部屋作成者を追加
+          {
+            userId: userStatus.userId,
+            userName: userStatus.userName,
+            isReady: true
           }
-        }
+        ],
+        isAllUsersReady: false
+      } as GameState
 
-        break
+      await kvSet(gameId, gameState)
+      break
+    }
+
+    // ユーザーの追加
+    case 'enter': {
+      // userIdまたはuserNameがなければエラー
+      if (!userStatus.userId || !userStatus.userName) {
+        return respondWithError('playerId not found', 404)
       }
 
-      default:
-        return respondWithError('Unknown action', 400)
+      gameState.users.push({
+        userId: userStatus.userId,
+        userName: userStatus.userName,
+        isReady: true
+      })
+
+      await handleSetGameState(gameState)
+      break
     }
-  })
+
+    // ユーザーの準備完了
+    case 'ready': {
+      // GameStateが存在しないなら終了
+      if (!gameState) break
+
+      if (userStatus) {
+        const user = gameState.users.find(
+          user => user.userId === userStatus.userId
+        )
+        if (user) {
+          // 参照渡し
+          user.isReady = userStatus.isReady
+          gameState.isAllUsersReady = getAllReady(gameState.users)
+          await handleSetGameState(gameState)
+        }
+      }
+      break
+    }
+  }
+  // 最後に変更内容を反映したGameStateまたはnull返す
+  return (await kvGet(gameId)) ?? null
 }
 
 // DELETE -------------------------------------------------
@@ -133,6 +131,7 @@ export async function handleDeleteGameState (req: NextRequest) {
     return respondWithError('Missing gameId', 400)
   }
 
-  const gameState = await kvDel(gameId)
-  return NextResponse.json(gameState, { status: 200 })
+  await kvDel(gameId)
+  // bodyなしで返却
+  return new NextResponse(null, { status: 204 })
 }
