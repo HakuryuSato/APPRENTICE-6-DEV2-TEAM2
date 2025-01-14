5e6d9b97a8512f186e3b9a714876129a7fa0cb59
HakuryuSato <r700540@gmail.com>
2025-01-14 14:37:28 +0900

    route.tsにDLETE用

--- src/app/api/game-state/route.ts
+++ src/app/api/game-state/route.ts
@@ -1,13 +1,18 @@
 import { NextRequest } from 'next/server';
 import { handleExternalApiRequest } from '@/utils/server/handleExternalApiRequest';
-import { handleGetGameState, handlePutGameState } from '@/utils/server/api/gameState';
+import { handleGetGameState, handlePOSTGameState,handleDeleteGameState } from '@/utils/server/api/gameState';
 
 export async function GET(req: NextRequest) {
   return handleExternalApiRequest(() => handleGetGameState(req));
 }
 
 export async function POST(req: NextRequest) {
-  return handleExternalApiRequest(() => handlePutGameState(req));
+  return handleExternalApiRequest(() => handlePOSTGameState(req));
 }
+
+export async function DELETE(req: NextRequest) {
+  return handleExternalApiRequest(() => handleDeleteGameState(req));
+}
+
 
 
