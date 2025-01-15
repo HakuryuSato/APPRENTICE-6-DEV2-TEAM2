5aa56c3f514ffce084579616500bb398b00a592d
HakuryuSato <r700540@gmail.com>
2025-01-15 14:55:55 +0900

    SIgnUpのテキスト一部変更、不要なconsole.log()を削除

--- src/components/top/TopPage/SignUp.tsx
+++ src/components/top/TopPage/SignUp.tsx
@@ -7,7 +7,6 @@ import { userNameAtom } from '@/atoms/state';
 
 export const SignUp: React.FC = () => {
   const [userName, setUserName] = useAtom(userNameAtom);
-  console.log(userName);
   const { inputText, setInputText, handleClickSubmit } = useSignUp(setUserName);
 
   return (
@@ -19,7 +18,7 @@ export const SignUp: React.FC = () => {
         gap: '16px',
       }}
     >
-      <h1>ようこそ、まずはニックネームを入力！ :D</h1>
+      <h1>ニックネームの入力</h1>
       <Input
         type="text"
         value={inputText}
