c2d0df055e88a036b5ed8b0384c669a29daae5a5
haruki1227041209 <haruki122704@gmail.com>
2025-01-15 19:40:10 +0900

    use clientを1つ削除

--- src/app/dev/satou/page.tsx
+++ src/app/dev/satou/page.tsx
@@ -1,12 +1,9 @@
-'use client'
-"use client";
-import { CountDown } from "@/components/game/GamePage/CountDown";
-
-
+'use client';
+import { CountDown } from '@/components/game/GamePage/CountDown';
 
 export default function SatouPage() {
-    const handleComplete = ()=>{
-        console.log('カウントダウン終了後に呼ばれる')
-    }
-    return <CountDown seconds={5} onZero={handleComplete} />;
+  const handleComplete = () => {
+    console.log('カウントダウン終了後に呼ばれる');
+  };
+  return <CountDown seconds={5} onZero={handleComplete} />;
 }
