ad7163c2843b3a3807eac94fcd8e93cea08eefc2
HakuryuSato <r700540@gmail.com>
2025-01-16 19:16:18 +0900

    GenerateImageRequest用の型を定義

--- src/types/GenerateImage.ts
+++ src/types/GenerateImage.ts
@@ -1,15 +1,14 @@
-import { GameState } from './GameState';
-
-export interface GenerateImageRequest {
-  prompt: string;//生成するためのプロンプトテキスト
-  n: number; // 生成する画像の数
-  size: string; // 例: "256x256"
-  userId : string; // ユーザーID
-  gameId: string; // ゲームID
-}
+import type { GameState } from './GameState';
+import type { UserStatus } from './UserStatus';
 
 export interface GenerateImage {
   url: string;
+}
+
+export interface GenerateImageRequest {
+  gameId: Pick<GameState, 'gameId'>;
+  userId: Pick<UserStatus, 'userId'>;
+  prompt: string;
 }
 
 export interface GeneratedImageProps extends GenerateImage {
