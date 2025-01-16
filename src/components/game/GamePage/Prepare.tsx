01ab3a239863d4e83f5208da84304555d1f19ffe
HakuryuSato <r700540@gmail.com>
2025-01-16 17:57:58 +0900

    PrepareでgameStateAtomを使用するよう修正

--- src/components/game/GamePage/Prepare.tsx
+++ src/components/game/GamePage/Prepare.tsx
@@ -1,24 +1,22 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { Button } from '@/components/ui/button';
-import { useAtom } from 'jotai';
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { useAtom } from "jotai";
 import {
-  userNameAtom,
   gamePageModeAtom,
+  gameStateAtom,
   userIdAtom,
-  gameStateAtom,
-} from '@/atoms/state';
-import { usePolling } from '@/hooks/game/GamePage/usePolling';
-import { selectAtom } from 'jotai/utils';
-import { useGameState } from '@/hooks/game/GamePage/useGameState';
+  userNameAtom,
+} from "@/atoms/state";
+import { usePolling } from "@/hooks/game/GamePage/usePolling";
+import { selectAtom } from "jotai/utils";
+import { useGameState } from "@/hooks/game/GamePage/useGameState";
 
-const gameIdSelector = selectAtom(gameStateAtom, (state) => state.gameId ?? '');
-const isAllUsersReadySelector = selectAtom(
-  gameStateAtom,
-  (state) => state.isAllUsersReady ?? false
-);
+// 特定のプロパティのみを監視
+const gameIdSelector = selectAtom(gameStateAtom, (state) => state.gameId ?? "");
+const isAllUsersReadySelector = selectAtom(gameStateAtom,(state) => state.isAllUsersReady ?? false);
 
 export const Prepare: React.FC = () => {
   const router = useRouter();
@@ -31,7 +29,7 @@ export const Prepare: React.FC = () => {
 
   const { startPolling, stopPolling } = usePolling(
     () => refreshGameState({ gameId }),
-    1000
+    1000,
   );
 
   useEffect(() => {
@@ -43,14 +41,14 @@ export const Prepare: React.FC = () => {
     if (isAllUsersReady) {
       stopPolling();
       const timeout = setTimeout(() => {
-        setTemporaryTopGameLayoutMode({ mode: 'generate' });
+        setTemporaryTopGameLayoutMode({ mode: "generate" });
       }, 3000);
       return () => clearTimeout(timeout);
     }
   }, [isAllUsersReady]);
 
   const handleClickBack = () => {
-    router.push('/');
+    router.push("/");
     // キャッシュの削除を追加する
   };
 
@@ -61,17 +59,19 @@ export const Prepare: React.FC = () => {
         <p className="font-semibold">Room: {gameId}</p>
       </div>
 
-      {!isAllUsersReady ? (
-        <h3 className="text-red-500 text-lg font-medium">
-          全員揃うまでお待ちください
-        </h3>
-      ) : (
-        <div className="text-center">
-          <p className="text-fly-purple font-bold text-xl animate-pop-in">
-            ゲームスタート
-          </p>
-        </div>
-      )}
+      {!isAllUsersReady
+        ? (
+          <h3 className="text-red-500 text-lg font-medium">
+            全員揃うまでお待ちください
+          </h3>
+        )
+        : (
+          <div className="text-center">
+            <p className="text-fly-purple font-bold text-xl animate-pop-in">
+              ゲームスタート
+            </p>
+          </div>
+        )}
 
       <Button onClick={handleClickBack}>戻る</Button>
     </div>
