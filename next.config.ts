506af3521381e8e60d9cf0afa8bc6cc2073c075c
traning8988 <142848531+traning8988@users.noreply.github.com>
2025-01-17 02:25:57 +0900

    ドメインを追加

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com", "picsum.photos"], // 許可する画像のホスト名を追加
+    domains: [
+      'images.unsplash.com',
+      'picsum.photos',
+      'oaidalleapiprodscus.blob.core.windows.net',
+    ], // 許可する画像のホスト名を追加
   },
 };
 
