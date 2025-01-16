a73eb1708ae7bb6c9f33c3514b96e46de74e5f47
traning8988 <142848531+traning8988@users.noreply.github.com>
2025-01-17 02:24:18 +0900

    promptsAtomをjotaiに追加

--- src/atoms/state.tsx
+++ src/atoms/state.tsx
@@ -3,6 +3,7 @@ import { atomWithStorage } from 'jotai/utils';
 export const userNameAtom = atomWithStorage('userName', '');
 export const gameIdAtom = atomWithStorage('gameId', '');
 export const userIdAtom = atomWithStorage('userId', '');
+export const promptsAtom = atomWithStorage<string[]>('prompt', []);
 
 //temporaryTopPageLayoutModeは　sign-up, select, create, enter
 export const topPageModeAtom = atomWithStorage('temporaryTopPageLayoutMode', {
