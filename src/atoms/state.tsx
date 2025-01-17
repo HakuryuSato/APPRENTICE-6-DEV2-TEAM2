6c329d1f5c4d2f33f5e7d743f2f215adbc72a911
traning8988 <142848531+traning8988@users.noreply.github.com>
2025-01-17 17:46:47 +0900

    jotaiにroundを追加

--- src/atoms/state.tsx
+++ src/atoms/state.tsx
@@ -4,6 +4,7 @@ export const userNameAtom = atomWithStorage('userName', '');
 export const gameIdAtom = atomWithStorage('gameId', '');
 export const userIdAtom = atomWithStorage('userId', '');
 export const promptsAtom = atomWithStorage<string[]>('prompt', []);
+export const roundAtom = atomWithStorage<number>('round', 1);
 
 //temporaryTopPageLayoutModeは　sign-up, select, create, enter
 export const topPageModeAtom = atomWithStorage('temporaryTopPageLayoutMode', {
