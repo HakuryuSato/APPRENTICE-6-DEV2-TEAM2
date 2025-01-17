dbc6d48de84e78f3f0684b080e4c9066fd2668a6
HakuryuSato <r700540@gmail.com>
2025-01-17 17:27:30 +0900

    layout.tsxで全ページに適用するThemeProviderを使用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Potta_One } from 'next/font/google';
 import './globals.css';
+import { ThemeProvider } from "@/components/theme-provider"
 
 
 const pottaOne = Potta_One({
@@ -19,12 +20,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
       // 文字フォントの指定
         className={`${pottaOne.className} antialiased bg-cover bg-center bg-fixed bg-[url('/kami.jpeg')]`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
