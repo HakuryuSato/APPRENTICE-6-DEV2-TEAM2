4345603f4760efab7051b83f2f2af9ab31365e15
haruki1227041209 <haruki122704@gmail.com>
2025-01-19 09:02:34 +0900

    selectページのUI修正

--- src/components/top/TopPage/Select.tsx
+++ src/components/top/TopPage/Select.tsx
@@ -1,23 +1,21 @@
-import React from "react";
-import { useAtom } from "jotai";
-import { topPageModeAtom, userNameAtom } from "@/atoms/state";
-import ThreeDButton from "@/components/3DButton/3DButton";
-import { Button } from "@/components/ui/button";
+import React from 'react';
+import { useAtom } from 'jotai';
+import { topPageModeAtom, userNameAtom } from '@/atoms/state';
+import ThreeDButton from '@/components/3DButton/3DButton';
+import { Button } from '@/components/ui/button';
 
 export const Select: React.FC = () => {
   const [userName] = useAtom(userNameAtom);
-  const [, setTemporaryTopPageLayoutMode] = useAtom(
-    topPageModeAtom,
-  ); // グローバルステートから取得
+  const [, setTemporaryTopPageLayoutMode] = useAtom(topPageModeAtom); // グローバルステートから取得
 
   const handleClickCreate = () => {
-    setTemporaryTopPageLayoutMode({ mode: "create" });
+    setTemporaryTopPageLayoutMode({ mode: 'create' });
   };
   const handleClickEnter = () => {
-    setTemporaryTopPageLayoutMode({ mode: "enter" });
+    setTemporaryTopPageLayoutMode({ mode: 'enter' });
   };
   const handleClickBack = () => {
-    setTemporaryTopPageLayoutMode({ mode: "sign-up" });
+    setTemporaryTopPageLayoutMode({ mode: 'sign-up' });
   };
 
   return (
@@ -33,21 +31,21 @@ export const Select: React.FC = () => {
       <p>ニックネーム: {userName}</p>
       <div
         style={{
-          display: "flex",
-          gap: "2rem",
-          justifyContent: "center",
-          marginBottom: "2rem",
+          width: '100%',
+          display: 'flex',
+          height: '30%', // ここで3Dボタンの高さ変更
+          gap: '2rem',
+          justifyContent: 'center',
+          marginBottom: '2rem',
         }}
       >
-        <div style={{ width: "200px", height: "200px" }}>
+        <div className="w-full h-auto flex flex-row justify-center items-center space-x-8 m-2">
           <ThreeDButton
             text="部屋を作る"
             handleEnter={handleClickCreate}
             initialImage="/images/SimpleHome.png"
             onHoverImage="/images/SimpleHome.png"
           />
-        </div>
-        <div style={{ width: "200px", height: "200px" }}>
           <ThreeDButton
             text="部屋に入る"
             handleEnter={handleClickEnter}
